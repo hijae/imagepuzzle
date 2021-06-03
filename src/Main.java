f9f0dd16ecdd7847c6149fbac055a3001445a0e7
Hijae song <hijae99@gmail.com>
2021-06-04 02:19:46 +0900

    정보보기창

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,48 @@
-import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 
 public class Main extends JFrame {
     public Main() {
-        setTitle("18114022 송희재");
+        setTitle("15퍼즐");
         createMenu(); // 메뉴 생성, 프레임에 삽입
         setSize(250,200);
         setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     private void createMenu() {
-
         JMenuBar mb = new JMenuBar();
-        String[] itemTitle1 = {"시작","정지","이미지 보기","정보보기"};
-        String[] itemTitle2 = {"이미지 불러오기","순서 섞기","이미지초기화"};
+        String[] itemName = {"게임","기능"};
+        String[][] itemTitle = {{"시작","정지","--","정보보기"},{"이미지 불러오기","순서 섞기","이미지초기화","이미지보기"}};
         MenuActionListener listener = new MenuActionListener();
-        JMenuItem [] menuItem1 = new JMenuItem [4];
-        JMenuItem [] menuItem2 = new JMenuItem [3];
-        JMenu screenMenu1 = new JMenu("게임");
-        for(int i=0;i<itemTitle1.length;i++) {
-            menuItem1[i] = new JMenuItem(itemTitle1[i]);
-            menuItem1[i].addActionListener(listener);
-            screenMenu1.add(menuItem1[i]);
+        JMenuItem [][] menuItem = new JMenuItem [2][4];
+        JMenu screenMenu[]=new JMenu[2];
+        for(int i=0;i<itemName.length;i++) {
+            screenMenu[i] = new JMenu(itemName[i]);
+            for (int j = 0; j < itemTitle[i].length; j++) {
+                if(itemTitle[i][j]=="--")
+                {
+                    screenMenu[i].addSeparator();
+                    continue;
+                }
+                menuItem[i][j] = new JMenuItem(itemTitle[i][j]);
+                menuItem[i][j].addActionListener(listener);
+                screenMenu[i].add(menuItem[i][j]);
+            }
+            mb.add(screenMenu[i]);
         }
-        mb.add(screenMenu1);
-
-        mb.add(screenMenu2);
-        JMenu screenMenu22 = new JMenu("2-2번");
-        screenMenu22.add(new JMenuItem("Open"));
-        screenMenu22.addSeparator(); // 분리선 삽입
-        screenMenu22.add(new JMenuItem("Color"));
-        screenMenu22.addSeparator(); // 분리선 삽입
-        screenMenu22.add(new JMenuItem("Line"));
-        screenMenu22.add(new JMenuItem("Rect"));
-        screenMenu22.add(new JMenuItem("Oval"));
-        screenMenu22.add(new JMenuItem("RndRect"));
-        screenMenu22.add(new JMenuItem("Text"));
-        screenMenu22.add(new JMenuItem("FreeLine"));
-
-        mb.add(screenMenu22);
-        JMenu screenMenu3 = new JMenu("3,4,5번");
-        screenMenu3.add(new JMenuItem("3.게임시작"));
-        screenMenu3.add(new JMenuItem("4.계산기"));
-        JMenuItem menujavaitem = new JMenuItem("5.자바란?");
-        Main.MenuActionListener listener = new Main.MenuActionListener();
-        menujavaitem.addActionListener(listener);
-        screenMenu3.add(menujavaitem);
-
-        mb.add(screenMenu3);
         setJMenuBar(mb);
     }
     class MenuActionListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+            String cmd = e.getActionCommand();
+            switch(cmd) {
+                case "시작":
 
+                case "정보보기" :
+                    new NewWindowInfo();
+                    break;
+            }
         }
     }
     public static void main(String [] args) {
