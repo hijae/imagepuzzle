20f36b753c944493a735ae03955b830df96c4aed
Hijae song <hijae99@gmail.com>
2021-06-09 01:39:34 +0900

    랜덤 횟수 수정

--- src/Main.java
+++ src/Main.java
@@ -69,7 +69,7 @@ public class Main extends JFrame {
         repaint();
     }
     void randommap() {
-        for(int i=0;i<3;i++)
+        for(int i=0;i<30;i++)
         {
             int r=(int)(Math.random()*4);
             switch (r) {
@@ -79,7 +79,6 @@ public class Main extends JFrame {
                 case 3 -> right();
             }
         }
-
     }
     void up()
     {
